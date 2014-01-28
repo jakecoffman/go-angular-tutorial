8b37eb84f77b01df28d53f5bfc552c7f1c79ae68
jake <jake@coffshire.com>
2014-01-27 19:43:15 -0600

    Actually tutorial 2 will start with websockets. This will be much more interesting for views.

--- tutorial_2/README.md
+++ tutorial_2/README.md
@@ -1,3 +1,18 @@
-Tutorial 3
+Tutorial 1
 ==========
 
+In this first tutorial we get up and running with a basic web server that
+serves any files in the local directory.
+
+How to run
+-------------
+
+* Install Go
+    * http://golang.org/doc/install
+* Download this project from github and unzip
+    * https://github.com/jakecoffman/golang-webapp-tutorial/archive/master.zip
+* Open a command window and `cd` to the tutorial_1 directory
+* Type `go run main.go` to start the server
+* Open a web browser and go to http://localhost/
+
+If you get a message that says "It works!" then it's working.
--- tutorial_2/main.go
+++ tutorial_2/main.go
@@ -1,113 +1,36 @@
 package main
 
 import (
-	"encoding/json"
-	"errors"
 	"flag"
 	"fmt"
-	"github.com/gorilla/mux"
-	"io/ioutil"
+	"github.com/gorilla/websocket"
 	"log"
 	"net/http"
-	"path/filepath"
-	"strings"
 )
 
-const blog = "blog/"
-
-var router = mux.NewRouter()
-
-type handlerError struct {
-	Error   error
-	Message string
-	Code    int
-}
-
-type handler func(w http.ResponseWriter, r *http.Request) (interface{}, *handlerError)
-
-func (fn handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	response, err := fn(w, r)
-	if err != nil {
-		log.Printf("ERROR: %v\n", err.Error)
-		http.Error(w, fmt.Sprintf(`{"error":"%s"}`, err.Message), err.Code)
+func wsHandler(w http.ResponseWriter, r *http.Request) {
+	// Taken from gorilla's website
+	conn, err := websocket.Upgrade(w, r, nil, 1024, 1024)
+	if _, ok := err.(websocket.HandshakeError); ok {
+		http.Error(w, "Not a websocket handshake", 400)
+		return
+	} else if err != nil {
+		log.Println(err)
 		return
 	}
+	log.Println("Succesfully upgraded connection")
 
-	bytes, e := json.Marshal(response)
-	if e != nil {
-		http.Error(w, "Error marshalling JSON", http.StatusInternalServerError)
-		return
-	}
-
-	w.Write(bytes)
-	log.Printf("%s %s %s", r.RemoteAddr, r.Method, r.URL)
-}
-
-func listEntries(w http.ResponseWriter, r *http.Request) (interface{}, *handlerError) {
-	files, e := filepath.Glob(blog + "*")
-	if e != nil {
-		return nil, &handlerError{e, "Error getting entries", http.StatusInternalServerError}
-	}
-
-	entries := make([]map[string]string, 0, len(files))
-	for _, value := range files {
-		name := strings.Replace(value, "\\", "/", -1) // in case of windows
-		name = strings.Replace(name, blog, "", 1)
-		entry := map[string]string{
-			"name": name,
+	for {
+		// Blocks until a message is read
+		messageType, p, err := conn.ReadMessage()
+		if err != nil {
+			return
 		}
-		entries = append(entries, entry)
-	}
-
-	return entries, nil
-}
-
-func getEntry(w http.ResponseWriter, r *http.Request) (interface{}, *handlerError) {
-	entry := mux.Vars(r)["entry"]
-	content, e := ioutil.ReadFile(blog + entry)
-	if e != nil {
-		return nil, &handlerError{e, "Could not find entry", http.StatusNotFound}
-	}
-
-	response := map[string]string{
-		"content": string(content),
-	}
-
-	return response, nil
-}
-
-func addEntry(w http.ResponseWriter, r *http.Request) (interface{}, *handlerError) {
-	data, e := ioutil.ReadAll(r.Body)
-	if e != nil {
-		return nil, &handlerError{e, "Could not read request", http.StatusBadRequest}
-	}
-
-	var payload map[string]string
-	e = json.Unmarshal(data, &payload)
-	if e != nil {
-		return nil, &handlerError{e, "Could not parse JSON", http.StatusBadRequest}
-	}
-	if !containsAll(payload, "name", "content") {
-		e = errors.New("JSON must contain 'name' and 'content'")
-		return nil, &handlerError{e, e.Error(), http.StatusBadRequest}
-	}
-
-	e = ioutil.WriteFile(blog+payload["name"], []byte(payload["content"]), 0644)
-	if e != nil {
-		return nil, &handlerError{e, "Could not write to disk", http.StatusInternalServerError}
-	}
-
-	return make(map[string]string), nil
-}
-
-func containsAll(m map[string]string, keys ...string) bool {
-	for _, key := range keys {
-		if _, ok := m[key]; !ok {
-			return false
+		log.Println(string(p))
+		if err = conn.WriteMessage(messageType, p); err != nil {
+			return
 		}
 	}
-	return true
 }
 
 func main() {
@@ -119,12 +42,8 @@ func main() {
 	// handle all requests by serving a file of the same name
 	fs := http.Dir(*dir)
 	fileHandler := http.FileServer(fs)
-	router.Handle("/", fileHandler)
-	router.Handle("/static", fileHandler)
-	router.Handle("/blog", handler(listEntries))
-	router.Handle("/blog/{entry}", handler(addEntry)).Methods("POST")
-	router.Handle("/blog/{entry}", handler(getEntry)).Methods("GET")
-	http.Handle("/", router)
+	http.Handle("/", fileHandler)
+	http.HandleFunc("/ws", wsHandler)
 
 	log.Printf("Running on port %d\n", *port)
 
