0aa2c470a8ca4497d943ff269815db24b5d9b4af
jake <jake@coffshire.com>
2014-02-01 08:42:25 -0600

    simplify

--- tutorial_3/main.go
+++ tutorial_3/main.go
@@ -2,16 +2,13 @@ package main
 
 import (
 	"encoding/json"
-	"errors"
 	"flag"
 	"fmt"
-	"github.com/gorilla/mux"
 	"io/ioutil"
 	"log"
 	"net/http"
-	"os"
-	"path/filepath"
-	"strings"
+
+	"github.com/gorilla/mux"
 )
 
 const blog = "blog/"
@@ -23,6 +20,13 @@ type handlerError struct {
 	Message string
 	Code    int
 }
+
+type book struct {
+	title  string
+	author string
+}
+
+var books = make([]book, 0)
 
 type handler func(w http.ResponseWriter, r *http.Request) (interface{}, *handlerError)
 
@@ -45,79 +49,57 @@ func (fn handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	log.Printf("%s %s %s", r.RemoteAddr, r.Method, r.URL)
 }
 
-func listEntries(w http.ResponseWriter, r *http.Request) (interface{}, *handlerError) {
-	files, e := filepath.Glob(blog + "*")
-	if e != nil {
-		return nil, &handlerError{e, "Error getting entries", http.StatusInternalServerError}
-	}
-
-	entries := make([]map[string]string, 0, len(files))
-	for _, value := range files {
-		name := strings.Replace(value, "\\", "/", -1) // in case of windows
-		name = strings.Replace(name, blog, "", 1)
-		entry := map[string]string{
-			"name": name,
-		}
-		entries = append(entries, entry)
-	}
-
-	return entries, nil
+func listBooks(w http.ResponseWriter, r *http.Request) (interface{}, *handlerError) {
+	return books, nil
 }
 
-func getEntry(w http.ResponseWriter, r *http.Request) (interface{}, *handlerError) {
-	entry := mux.Vars(r)["entry"]
-	content, e := ioutil.ReadFile(blog + entry)
-	if e != nil {
-		return nil, &handlerError{e, "Could not find entry", http.StatusNotFound}
+func getBook(w http.ResponseWriter, r *http.Request) (interface{}, *handlerError) {
+	title := mux.Vars(r)["title"]
+	b, index := getBookByTitle(title)
+
+	if index < 0 {
+		return nil, &handlerError{nil, "Could not find entry " + title, http.StatusNotFound}
 	}
 
-	response := map[string]string{
-		"content": string(content),
-	}
-
-	return response, nil
+	return b, nil
 }
 
-func addEntry(w http.ResponseWriter, r *http.Request) (interface{}, *handlerError) {
+func addBook(w http.ResponseWriter, r *http.Request) (interface{}, *handlerError) {
 	data, e := ioutil.ReadAll(r.Body)
 	if e != nil {
 		return nil, &handlerError{e, "Could not read request", http.StatusBadRequest}
 	}
 
-	var payload map[string]string
+	var payload book
 	e = json.Unmarshal(data, &payload)
 	if e != nil {
 		return nil, &handlerError{e, "Could not parse JSON", http.StatusBadRequest}
 	}
-	if !containsAll(payload, "name", "content") {
-		e = errors.New("JSON must contain 'name' and 'content'")
-		return nil, &handlerError{e, e.Error(), http.StatusBadRequest}
-	}
 
-	e = ioutil.WriteFile(blog+payload["name"], []byte(payload["content"]), 0644)
-	if e != nil {
-		return nil, &handlerError{e, "Could not write to disk", http.StatusInternalServerError}
-	}
+	books = append(books, payload)
 
 	return make(map[string]string), nil
 }
 
-func removeEntry(w http.ResponseWriter, r *http.Request) (interface{}, *handlerError) {
-	entry := mux.Vars(r)["entry"]
-	e := os.Remove(blog + entry)
-	if e != nil {
-		return nil, &handlerError{e, "Could not find entry", http.StatusNotFound}
+func removeBook(w http.ResponseWriter, r *http.Request) (interface{}, *handlerError) {
+	title := mux.Vars(r)["title"]
+	_, index := getBookByTitle(title)
+
+	if index < 0 {
+		return nil, &handlerError{nil, "Could not find entry " + title, http.StatusNotFound}
 	}
+
+	books = books[:index+copy(books[index:], books[index+1:])]
 	return make(map[string]string), nil
 }
 
-func containsAll(m map[string]string, keys ...string) bool {
-	for _, key := range keys {
-		if _, ok := m[key]; !ok {
-			return false
+func getBookByTitle(title string) (book, int) {
+	for i, b := range books {
+		if b.title == title {
+			return b, i
 		}
 	}
-	return true
+	return book{}, -1
 }
 
 func main() {
@@ -131,10 +113,10 @@ func main() {
 	fileHandler := http.FileServer(fs)
 	router.Handle("/", fileHandler)
 	router.Handle("/static", fileHandler)
-	router.Handle("/blog", handler(listEntries))
-	router.Handle("/blog/{entry}", handler(addEntry)).Methods("POST")
-	router.Handle("/blog/{entry}", handler(getEntry)).Methods("GET")
-	router.Handle("/blog/{entry}", handler(removeEntry)).Methods("DELETE")
+	router.Handle("/blog", handler(listBooks))
+	router.Handle("/blog/{entry}", handler(addBook)).Methods("POST")
+	router.Handle("/blog/{entry}", handler(getBook)).Methods("GET")
+	router.Handle("/blog/{entry}", handler(removeBook)).Methods("DELETE")
 	http.Handle("/", router)
 
 	log.Printf("Running on port %d\n", *port)
